54173fcf2f3f401f396be73cce7a0736aa4c9531
Ishan Roy Choudhury <ishanrch@ucla.edu>
2015-11-18 22:13:29 -0800

    memory caching

--- app/controllers/nearby_controller.rb
+++ app/controllers/nearby_controller.rb
@@ -9,12 +9,20 @@ class NearbyController < ApplicationController
       @@distance_range = params[:radius].to_f
     end
 
-    @nearbyTweets = Tweet.where(
+    key = 'nearbyController' << params[:lat].to_str << params[:lon].to_str
+
+    if (Rails.cache.exist?(key))
+      @nearbyTweets = Rails.cache.read(key)
+      puts(@nearbyTweets)
+    else
+      @nearbyTweets = Tweet.where(
         latitude: (params[:lat].to_f-@@distance_range .. params[:lat].to_f+@@distance_range),
         longitude: (params[:lon].to_f-@@distance_range .. params[:lon].to_f+@@distance_range))
           .joins('JOIN users ON tweets.FK_user_id=users.twitter_id')
           .select('tweets.text, tweets.latitude, tweets.longitude, tweets.location_full_name, tweets.source, users.name, users.screen_name')
-
+      Rails.cache.write(key, @nearbyTweets)
+    end
+    puts(key)
     render json: @nearbyTweets
   end
 
