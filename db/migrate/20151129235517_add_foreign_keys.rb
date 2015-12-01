6e8a43366d8304982d4730b40f48ab0491dd84b9
Nisarg Shah <snisarg@gmail.com>
2015-11-30 16:01:40 -0800

    DB name added by accident

--- db/migrate/20151129235517_add_foreign_keys.rb
+++ db/migrate/20151129235517_add_foreign_keys.rb
@@ -3,7 +3,7 @@ class AddForeignKeys < ActiveRecord::Migration
     add_foreign_key :hashtag_lists, :hashtags, column: :FK_hashtag_id, primary_key: :id
     add_foreign_key :hashtag_lists, :tweets, column: :FK_tweet_id, primary_key: :id
     remove_column :users, :id
-    execute "ALTER TABLE mint.users ADD PRIMARY KEY (twitter_id);"
+    execute "ALTER TABLE users ADD PRIMARY KEY (twitter_id);"
     add_foreign_key :tweets, :users, column: :FK_user_id, primary_key: :twitter_id
     add_foreign_key :mentions, :tweets, column: :FK_tweet_id, primary_key: :id
     add_foreign_key :mentions, :users, column: :FK_user_id, primary_key: :twitter_id
